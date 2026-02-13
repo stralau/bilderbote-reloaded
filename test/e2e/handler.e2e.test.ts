d22a2df0ebd10ebbbc7778e4cd6c8f0709c731b6
Daniel Moor <daniel.moor@softwaretechnik.berlin>
2026-02-13 16:38:15 +0100

    Verify Mastodon post in e2e test

--- test/e2e/handler.e2e.test.ts
+++ test/e2e/handler.e2e.test.ts
@@ -1,7 +1,29 @@
 import {handler} from "../../src/handler.js";
+import * as dotenv from "dotenv";
+import * as Mastodon from "tsl-mastodon-api";
+
+dotenv.config();
+
+const mastodon = new Mastodon.API({
+  access_token: process.env.MASTODON_ACCESS_TOKEN,
+  api_url: `${process.env.MASTODON_INSTANCE_URL}/api/v1/`
+})
+
+async function getLatestPost() {
+  const account = await mastodon.get("accounts/verify_credentials")
+    .then(r => r.json as any)
+
+  const posts = await mastodon.getStatuses(account.id, {limit: 1}).then(s => s.json)
+  return posts[0]
+}
 
 test('posts an image successfully', async () => {
+  const postBefore = await getLatestPost()
+
   const result = await handler({})
 
   expect(result.statusCode).toBe(200)
+
+  const postAfter = await getLatestPost()
+  expect(postAfter.id).not.toBe(postBefore.id)
 }, 30_000)
