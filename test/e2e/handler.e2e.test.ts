d53be533f0ec91cafee8c915e187a49398872e60
Daniel Moor <daniel.moor@softwaretechnik.berlin>
2026-02-13 16:40:22 +0100

    Verify Bluesky post in e2e test

--- test/e2e/handler.e2e.test.ts
+++ test/e2e/handler.e2e.test.ts
@@ -1,6 +1,7 @@
 import {handler} from "../../src/handler.js";
 import * as dotenv from "dotenv";
 import * as Mastodon from "tsl-mastodon-api";
+import {AtpAgent} from "@atproto/api";
 
 dotenv.config();
 
@@ -9,7 +10,9 @@ const mastodon = new Mastodon.API({
   api_url: `${process.env.MASTODON_INSTANCE_URL}/api/v1/`
 })
 
-async function getLatestPost() {
+const bluesky = new AtpAgent({service: 'https://bsky.social'})
+
+async function getLatestMastodonPost() {
   const account = await mastodon.get("accounts/verify_credentials")
     .then(r => r.json as any)
 
@@ -17,13 +20,28 @@ async function getLatestPost() {
   return posts[0]
 }
 
+async function getLatestBlueskyPost() {
+  await bluesky.login({identifier: process.env.BLUESKY_USERNAME, password: process.env.BLUESKY_PASSWORD})
+
+  const feed = await bluesky.getAuthorFeed({
+    actor: process.env.BLUESKY_IMAGE_HANDLE,
+    limit: 1,
+    filter: "posts_no_replies"
+  })
+  return feed.data.feed[0]
+}
+
 test('posts an image successfully', async () => {
-  const postBefore = await getLatestPost()
+  const mastodonBefore = await getLatestMastodonPost()
+  const blueskyBefore = await getLatestBlueskyPost()
 
   const result = await handler({})
 
   expect(result.statusCode).toBe(200)
 
-  const postAfter = await getLatestPost()
-  expect(postAfter.id).not.toBe(postBefore.id)
+  const mastodonAfter = await getLatestMastodonPost()
+  expect(mastodonAfter.id).not.toBe(mastodonBefore.id)
+
+  const blueskyAfter = await getLatestBlueskyPost()
+  expect(blueskyAfter.post.cid).not.toBe(blueskyBefore.post.cid)
 }, 30_000)
