9eb5fb0a28365845695f12161d22212f70af936e
Daniel Moor <daniel.moor@softwaretechnik.berlin>
2026-02-16 17:41:13 +0100

    Readability

--- src/util/Retry.ts
+++ src/util/Retry.ts
@@ -13,14 +13,12 @@ export async function retry<T>(r: Retry<T>): Promise<Result<T>> {
     return Result.err(new Error("Number of attempts must be at least 1"))
   }
 
+  const result: Result<T> = await Result.tryAsync(r.fn);
   if (r.attempts == 1) {
-    return Result.tryAsync(r.fn)
-      .then(r => r.mapError(e => new Error(`Gave up retrying, last error: ${e.message}`)))
+    return result.mapError(e => new Error(`Failed with error: ${e.message}`))
   }
 
-  const result = await Result.tryAsync(r.fn)
   if (result.r.success === true || r.isFatal && r.isFatal(result.r.error)) return result
   console.log(`Failed with error: ${result.r.error.message}, retrying...`)
   return retry({...r, attempts: r.attempts - 1})
-
 }
